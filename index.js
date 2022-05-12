6b0c3878750d0a2397f326899030632f44176a4d
Areliz Oyarce <arelizuwc@gmail.com>
2022-05-12 10:08:20 -0500

    creation funtion to determine if the path is absolute closed #2

--- index.js
+++ index.js
@@ -1,5 +1,12 @@
-module.exports = () => {
-  //este comentario es de prueba
-  //Brach comprobada
-  //Comprobación de cerrar issues
+import fs from 'fs';
+import path from 'path';
+const mdlinks = () => {
+
 };
+
+//Función para determinar si la ruta es absoluta o no
+const determinateAbsolutePath = (pathRoot)=> {
+  return path.isAbsolute(pathRoot);
+};
+
+console.log(determinateAbsolutePath('//server'));
