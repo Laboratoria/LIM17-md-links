903d62ed0c79579abf1cfad21d1de87c69ec2bd4
Areliz Oyarce <arelizuwc@gmail.com>
2022-05-16 10:55:35 -0500

    add validatePath function to know if the path exists closed #4

--- index.js
+++ index.js
@@ -1,16 +1,19 @@
 import fs from 'fs';
 import path from 'path';
-const mdlinks = () => {
+const mdlinks = () => {};
 
-};
-
+const ruta = 'archivosdeprueba';
 //Función para determinar si la ruta es absoluta o no
 const determinateAbsolutePath = (pathRoot)=> path.isAbsolute(pathRoot);
-console.log(determinateAbsolutePath('dir1/arc1. fil'));
+console.log(determinateAbsolutePath(ruta));
 
 //Función para convertir ruta en absoluta
 const createAbsolutePath = (pathRoot)=> path.resolve(pathRoot);
-console.log(createAbsolutePath('dir1/arc1.fil'));
+console.log(createAbsolutePath(ruta));
+
+//Función para comprovar si la ruta existe o no
+const validatePath = (pathRoot) => fs.existsSync(pathRoot);
+console.log(validatePath(ruta));
 
 
 
