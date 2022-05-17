f2770f60a2c2eb9974569d8d50818c5512a19269
Areliz Oyarce <arelizuwc@gmail.com>
2022-05-17 11:20:27 -0500

    add test for user story one closed #5

--- test/md-links.spec.js
+++ test/md-links.spec.js
@@ -1,10 +1,21 @@
-const mdLinks = require('../');
+import { determinateAbsolutePath, createAbsolutePath,validatePath} from '../api.js'
 
-
-describe('mdLinks', () => {
-
-  it('should...', () => {
-    console.log('FIX ME!');
+const route = './archivosdeprueba';
+const absoluteRoute= 'C:\\LABORATORIA PROYECTOS\\P4-MDLINKS\\LIM017-md-links\\archivosdeprueba';
+describe('validatePath', () => {
+  it('should be return true if the path exists', () => {
+    expect(validatePath(route)).toBe(true);
   });
+});
 
+describe('determinateAbsolutePath', () => {
+  it('should be return false for absolute path', () => {
+    expect(determinateAbsolutePath(route)).toBe(false);
+  });
+});
+
+describe('createAbsolutePath', () => {
+  it('should be return a absolute path', () => {
+    expect(createAbsolutePath(route)).toBe(absoluteRoute);
+  });
 });
