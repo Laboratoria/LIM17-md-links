949b3f9de438d8022a50b39919522e302ec9540c
Areliz Oyarce <arelizuwc@gmail.com>
2022-05-19 16:38:52 -0500

    add isDirectory or isFile function for know if the path is a document or a directory Closed #7

--- api.js
+++ api.js
@@ -12,3 +12,29 @@ export const createAbsolutePath = (pathRoot)=> path.resolve(pathRoot);
 
 //Función para comprovar si la ruta existe o no
 export const validatePath = (pathRoot) => fs.existsSync(pathRoot);
+ 
+//Funciones para saber si el path es directorio o file
+
+export const ifIsFile = (pathRoot)=> {
+    const statsObj = fs.statSync(pathRoot);
+    return statsObj.isFile();
+}
+
+export const ifIsDirectory = (pathRoot)=> {
+    const statsObj = fs.statSync(pathRoot);
+    return statsObj.isDirectory();
+}
+
+export const readaPathFile= (pathRoot)=> console.log(fs.readFileSync(pathRoot));
+
+export const readaPathDirectory= (pathRoot)=> {
+    pathRoot.forEach(() => {
+        fs.readdir(pathRoot, function (err, archivos) {
+            if (err) {
+            onError(err);
+            return;
+            }
+            console.log(archivos);
+            });;
+    })
+}
--- index.js
+++ index.js
@@ -1,8 +1,16 @@
-import { determinateAbsolutePath, createAbsolutePath, validatePath} from './api.js'
+import { determinateAbsolutePath, createAbsolutePath, validatePath, ifIsDirectory, ifIsFile,readaPathDirectory,readaPathFile} from './api.js'
 const mdlinks = (route) => {
 validatePath(route)=== true ? console.log('Ruta existente') : console.log('Ruta no existente. Ingrese otra ruta.');
 determinateAbsolutePath(route)=== true ? console.log('La ruta es absoluta') : console.log('La ruta es relativa... Convirtiendo');
 console.log('la ruta relativa es : ' + createAbsolutePath(route));
+if(ifIsDirectory(route)=== true){
+    readaPathDirectory(route);
+}else{
+    readaPathFile(route);
+}
+// if(ifIsFile(route)=== true){
+//     readaPathFile(route)
+// }
 };
 
 mdlinks('archivosdeprueba');
