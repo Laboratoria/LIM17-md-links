df2491bd9ed0117fa3faf45e8157bea4d50f87b0
LissetteMH <lissette.machaca@gmail.com>
2022-05-23 15:48:36 -0500

    implementando tests de las primeras  operacionesAPI

--- operacionesApi.js
+++ operacionesApi.js
@@ -4,13 +4,13 @@ import path from 'path';
 // *********** VERIFICA SI EXISTE LA RUTA true or false ***********
 const existsPath = (route) => fs.existsSync (route);
 
-// *********** CONVIERTA A UNA RUTA ABSOLUTA ***********
+// *********** CONVIERTA A UNA RUTA ABSOLUTA **********************
 const pathIsAbsolute = (route) => path.isAbsolute(route) ? route : path.resolve(route);
 
-// *********** EL PATH ES UN DIRECTORIO? ***********
+// *********** EL PATH ES UN DIRECTORIO? **************************
 const pathIsAdirectory = (route) => fs.lstatSync(route).isDirectory();
 
-// *********** EL PATH ES UN ARCHIVO? ***********
-const pathIsFile = (route) => fs.statSync(route).isFile();
+// *********** EL PATH ES UN ARCHIVO? *****************************
+const pathIsAfile = (route) => fs.statSync(route).isFile();
 
-export default { existsPath, pathIsAbsolute, pathIsAdirectory, pathIsFile }
+export default { existsPath, pathIsAbsolute, pathIsAdirectory, pathIsAfile }
