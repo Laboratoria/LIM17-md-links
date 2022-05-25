8151e0495dd494e5cefa88782349303283b015d4
CarmenMerino <98608303+CarmenMerino@users.noreply.github.com>
2022-05-25 10:53:48 -0500

    primeros pasos

--- index.js
+++ index.js
@@ -1,3 +1,19 @@
+const fs = require('node:fs/promises');
+const http = require('node:https')
+
+fs.readFile('links.md', 'utf-8')
+.then((contenidos) => {
+  let links = contenidos.split('\n')
+  for(let i=0; i<links.length; i++){
+    console.log((i+1)+':'+links[i])
+    http.get(links[i], (res) => {
+      console.log(res.statusCode)
+    })
+  }
+})
+  
+
+
 module.exports = () => {
   // ...
 };
---
+++ links.md
@@ -0,0 +1,4 @@
+https://www.w3schools.com/nodejs/nodejs_filesystem.asp
+https://www.w3schools.com/nodejs/shownodejs.asp?filename=demo_readfile
+https://nodejs.org/api/synopsis.html#
+https://github.com/abcxyz/LIM017-md-links
