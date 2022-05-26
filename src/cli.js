79e799a0325063ca7541ee97bb517d8d1009989a
Emilce Toledo <toledo.emilce.a@gmail.com>
2022-05-25 23:48:40 -0500

    cambios

--- src/cli.js
+++ src/cli.js
@@ -1,8 +1,9 @@
 #!/usr/bin/env node
 const {mdLinks} = require('./mdLinks.js');
 
-const [a, b, ...args]= process.argv;
+// const [a, b, ...args]= process.argv;
+// console.log(args, args[0], args.includes('--validate'))
 
-console.log(args, args[0], args.includes('--validate'))
+// mdLinks(args[0], { validate: args.includes('--validate')})
 
-mdLinks(args[0], { validate: args.includes('--validate')})
+console.log(process.argv)
