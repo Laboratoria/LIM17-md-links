7639fc7a552c052750ae1a28c5e7acd59216b8b1
Emilce Toledo <toledo.emilce.a@gmail.com>
2022-05-25 23:06:34 -0500

    Function that contains the state of the links

--- src/index.js
+++ src/index.js
@@ -1,15 +1,17 @@
-const fs = require("fs");
-const path = require("path");
+const fs = require('fs');
+const path = require('path');
+const fetch = require('node-fetch');
+
 let inputRoute = process.argv[2];
 
 const absolutePath = (inputRoute) => path.isAbsolute(inputRoute) ? inputRoute : path.resolve(inputRoute);
-const existsRoute = (inputRoute) => fs.existsSync(inputRoute)? inputRoute : "NA";
+const existsRoute = (inputRoute) => fs.existsSync(inputRoute)? inputRoute : 'NA';
 const isFolder = (inputRoute) => fs.statSync(inputRoute).isDirectory();
 
 const getFileFolder = (inputRoute) =>{
 let arrCotainer = [];
 let arrContainerFalse=[];
-if (isFolder(inputRoute) === false && path.extname(inputRoute) === ".md") {
+if (isFolder(inputRoute) === false && path.extname(inputRoute) === '.md') {
   arrCotainer.push(inputRoute);
 } else if(isFolder(inputRoute) === false ){
   arrContainerFalse.push(inputRoute);
@@ -37,15 +39,43 @@ const getLinks = (inputFile) => {
 
       const dataLinks = {
         href: hrefLinks,//href: URL encontrada.
-        text: textLinks,// text: Texto que aparecía dentro del link
+        text: textLinks,// text: Texto que aparecía dentro del link.
         file: absolutePath(inputFile)// file: Ruta del archivo donde se encontró el link.
       }
       return dataLinks
     });
   };
+
   const arrLinksMd = getFileFolder(inputRoute);
   const linksCollection = arrLinksMd.map(file=> getLinks(file));
-  console.log(linksCollection);
+  
+  const linksStatus = (linksCollection) => {
+    const arrStatus = linksCollection[0].map((el) => {
+      const fetchObj = fetch(el.href)
+        .then((res) => {
+          const msgStatus = res.status >= 200 && res.status <= 299 ? 'ok' : 'fail';
+          const objStatus={
+            href: el.href,//href: URL encontrada.
+            text: el.text,// text: Texto que aparecía dentro del link.
+            file: el.file,// file: Ruta del archivo donde se encontró el link.
+            status: res.status,// Código de respuesta HTTP.
+            ok: msgStatus, //Mensaje 'fail' en caso de fallo u 'ok' en caso de éxito.
+          };
+          return objStatus;
+        })
+        .catch(() => ({
+          href: el.href,
+          text: el.text,
+          file: el.file,
+          status: 'Fail: Your request failed',
+          ok: 'fail',
+        }));
+      return fetchObj;
+    });
+    return Promise.all(arrStatus); 
+  };
+
+  linksStatus(linksCollection).then(el => console.log(el))
 
 module.exports = {
   existsRoute,
