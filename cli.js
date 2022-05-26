a2ae9a1a4ec18a16558738802b13c4c656fc755b
Areliz Oyarce <arelizuwc@gmail.com>
2022-05-26 16:45:35 -0500

    add cli.js, update package.json and start new Promise md-Links

---
+++ cli.js
@@ -0,0 +1,5 @@
+#!/usr/bin/env node
+
+// eslint-disable-next-line no-unused-vars
+const [,, ...args] = process.argv
+console.log(`Hello Mundo ${args}`)
--- index.js
+++ index.js
@@ -1,15 +1,36 @@
-import {  readaPathDirectory, getLinksFileMD, readaPathFile, getLinksofDirectory, determinateAbsolutePath, createAbsolutePath, validatePath, ifIsDirectory, ifIsFile } from './api.js'
-export const mdlinks = (route) => {
-  validatePath(route) === true ? console.log('Ruta existente') : console.log('Ruta no existente. Ingrese otra ruta.')
-  determinateAbsolutePath(route) === true ? console.log('La ruta es absoluta') : console.log('La ruta es relativa... Convirtiendo')
-  console.log('la ruta relativa es : ' + createAbsolutePath(route))
-  if (ifIsDirectory(route) === true) {
-    console.log(getLinksofDirectory(readaPathDirectory(route), route))
-  }
-  if (ifIsFile(route)) {
-    console.log(getLinksFileMD(readaPathFile(route), route))
-  }
-}
+import {
+  readaPathDirectory,
+  getLinksFileMD,
+  readaPathFile,
+  getLinksofDirectory,
+  determinateAbsolutePath,
+  createAbsolutePath,
+  validatePath,
+  ifIsDirectory,
+  ifIsFile
+} from './api.js'
+// export const mdlinks = (route) => {
+//   validatePath(route) === true ? console.log('Ruta existente') : console.log('Ruta no existente. Ingrese otra ruta.')
+//   determinateAbsolutePath(route) === true ? console.log('La ruta es absoluta') : console.log('La ruta es relativa... Convirtiendo')
+//   console.log('la ruta relativa es : ' + createAbsolutePath(route))
+//   if (ifIsDirectory(route) === true) {
+//     console.log(getLinksofDirectory(readaPathDirectory(route), route))
+//   }
+//   if (ifIsFile(route)) {
+//     console.log(getLinksFileMD(readaPathFile(route), route))
+//   }
+// }
 
 // mdlinks('exampleFileMD.md')
-mdlinks('archivosdeprueba')
+
+export const mdLinks = (path, option) => {
+  return new Promise((resolve, reject) => {
+    if (validatePath(path)) {
+      resolve('the path exists')
+    } else {
+      reject('Not valid')
+    }
+  })
+}
+
+mdLinks('archivosdepruebas')
