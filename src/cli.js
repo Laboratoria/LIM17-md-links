700677580023b264ae1966692dca17a9da17de28
Emilce Toledo <toledo.emilce.a@gmail.com>
2022-05-27 16:23:44 -0500

    creating CLI

--- src/cli.js
+++ src/cli.js
@@ -7,46 +7,30 @@ const stats = args.includes('--stats');
 const validate = args.includes('--validate');
 const inputArgs = args.length;
 
+if(inputArgs === 0 && !route){
+ const mngError ='Enter the route';
+ console.log(mngError)
 
-console.log(process.argv)
-if(inputArgs === 0){
- console.log('No ingresaste la ruta')
-}
+} else if(inputArgs === 1 && !validate && !stats){  
+   const arrObj= mdLinks(route, false);
+   const notValidate = arrObj.forEach(el =>{ el.href, el.text, el.file});
+   console.log(notValidate)
 
-if(inputArgs === 1){
-  console.log('href, text, file')
-  // mdLinks(route)
-  //   .then(links => {
-  //       links
-  //     // => [{ href, text, file }, ...]
-  //   })
-  //   .catch(console.error);
-}
-
-if(inputArgs === 2){
-  console.log('Solo --validate ')
-  if(validate && !stats){
- 
+} else if(inputArgs === 2 && validate && !stats){
   console.log(' href, text, file, status, ok')
-//   mdLinks(inputRoute, { validate: true })
-//   .then(links => {
-//     // => [{ href, text, file, status, ok }, ...]
-//   })
-//   .catch(console.error);
-  }
+   mdLinks(route, true)
+  .then(links => { console.log(links)
+  })
+  .catch(console.error);
 
-if(!validate && stats){
-  console.log('Total Unique')
-}
+} else if(inputArgs === 2 && !validate && stats){
+   console.log('Total Unique')
 
-}
-
-
-if(validate && stats){
+} else if(inputArgs === 2 && validate && stats){
     console.log('Total Unique Broken')
-//     mdLinks(inputRoute, { validate: true })
-//   .then(links => {
-//     // stats
-//   })
-//   .catch(console.error);
+  mdLinks(inputRoute, true )
+  .then(links => {
+    // stats
+  })
+  .catch(console.error);
 }
--- src/mdLinks.js
+++ src/mdLinks.js
@@ -1,14 +1,20 @@
 const {  
-  existsRoute,
   absolutePath,
-  getFileFolder,
-  getLinks,
-  linksStatus 
+  existsRoute,
+  objLinks,
+  linksStatus
 }= require('./index.js')
 
-//Función Principal
-
 const mdLinks = (route, options) =>{
+  const inputPath = absolutePath(route);
+  const exitsPath = existsRoute(inputPath);
+  const getObjLinks = objLinks(exitsPath);
+  if(options){
+   return linksStatus(getObjLinks)
+  }
+  else {
+   return  objLinks(exitsPath);
+  }
 }
 
-  module.exports = {mdLinks};
+module.exports = {mdLinks};
