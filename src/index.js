d0df62c0e9938d314cb9831d3cb2711fb5c13296
Juana-89 <juana.lc@hotmail.es>
2022-05-27 23:11:09 -0500

    First user story: Know if a route exists

--- src/index.js
+++ src/index.js
@@ -2,35 +2,35 @@ const fs = require('fs'); // Requiriendo módulo que nos permita interactuar con
 const path = require('path'); // Módulo que nos permite saber el tipo de extensión de un archivo 
 const chalk = require('chalk'); // módul
 //Leer archivo indicado
-// fs.readFile('../prueba/prueba.js','utf-8', (error,data) => {
-//     if(error){
-//     console.log(error);
-//     }else{
-//     console.log(data)}
-// });
+fs.readFile('../prueba/prueba.js','utf-8', (error,data) => {
+    if(error){
+    console.log(error);
+    }else{
+    console.log(data)}
+});
 
-// // //Averiguar extensión de un archivo
-// console.log(path.extname('pruebaccxxxx.md'))
+//Averiguar extensión de un archivo
+console.log(path.extname('pruebaccxxxx.md'))
 
-// //Obtener contenido de un directorio
-// fs.readdir('../prueba', (error, files) => {
-//     if(error){
-//         throw error;
-//     }
-// console.log('Listar directorio: ' + files)
-//     })
+//Obtener contenido de un directorio
+fs.readdir('../prueba', (error, files) => {
+    if(error){
+        throw error;
+    }
+console.log('Listar directorio: ' + files)
+    })
 
-// // //Unir dos rutas
-// let path1 = path.join('prueba', 'soyunloco.md');
-// console.log('Uniendo la ruta: ' + path1)
+//Unir dos rutas
+let path1 = path.join('prueba', 'soyunloco.md');
+console.log('Uniendo la ruta: ' + path1)
 
-// //crear archivos
-// fs.writeFile('.prueba.md', 'agregando cosas', function (err) {
-//     if (err) {
-//         console.log(err)
-//     }
-//     console.log('creado')
-// })
+//crear archivos
+fs.writeFile('.prueba.md', 'agregando cosas', function (err) {
+    if (err) {
+        console.log(err)
+    }
+    console.log('creado')
+})
 
 // Convertir a una ruta en absoluta
 const absolutePath = (file) => (path.isAbsolute(file)) ? file : path.resolve(file);
