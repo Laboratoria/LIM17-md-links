7399ee4a2b9f4e22c2fa0c5041b9ef0e2aadc955
Juana-89 <juana.lc@hotmail.es>
2022-06-06 11:32:55 -0500

    Adding changes HU2, know if md files have links

--- src/api.js
+++ src/api.js
@@ -1,4 +1,75 @@
-// const { absolutePath, pathExists, isDirectoryOrFile } = require('./index.js')
-// const mdLinks = (path, options) =>{
-    
-// }
+const fs = require('fs'); // Módulo que nos permita interactuar con directorios y archivos: file system
+const path = require('path'); // Módulo que nos permite saber el tipo de extensión de un archivo
+const marked = require('marked');
+const colors = require('colors'); // Módulo que da color a los comandos de la terminal
+colors.enable();
+// Convertir a una ruta en absoluta
+const absolutePath = (route) => (path.isAbsolute(route)) ? route : path.resolve(route);
+//console.log('Convirtiendo a una ruta absoluta con "absolutePath"' + absolutePath('./prueba/prueba'));
+
+// Saber si una ruta existe
+const pathExists = (route) => fs.existsSync(route) ? route : 'Path no exists';
+//console.log('¿Existe la ruta? ' + pathExists('1111.js'))
+
+// Saber si es un directorio
+const isDirectory = (route) => fs.statSync(route).isDirectory();
+//console.log('¿Es directorio? ' + isDirectory('C:\\Users\\USER\\Desktop\\LIM017-md-links\\src\\index.js'))
+
+// Saber si es un archivo
+const isFile = (route) => fs.statSync(route).isFile();
+//console.log('¿Es archivo? ' + isFile('C:\\Users\\USER\\Desktop\\LIM017-md-links\\src\\index.js'))
+
+// Conocer la extensión de un archivo
+const extFile = (file) => path.extname(file);
+//console.log(extFile('archivo.md'))
+
+// Buscar archivos
+const readDir = (route) => fs.readdirSync(route);
+
+// Lee Archivos
+const readFile = (file) => fs.readFileSync(file, 'utf-8');
+
+// Unir dos rutas
+const joinTwoPaths = (route) => {return readDir(route).map((element) => path.join(route, element))};
+
+//Validar directorio sino listar los archivos que encuentre y hacer la búsqueda de archivos .md
+const searchDirectoryWithFilesMD = (route) => {
+    let arrayFilesMD = [];
+    if(isFile(absolutePath(route)) && extFile(absolutePath(route)) === '.md'){
+        arrayFilesMD.push(absolutePath(route))
+    } else if(isDirectory(absolutePath(route))){
+        joinTwoPaths(absolutePath(route)).forEach(element => {
+        const filesMD = searchDirectoryWithFilesMD(element)
+        arrayFilesMD = arrayFilesMD.concat(filesMD)
+    });
+    }
+    else{
+        return 'No exists directory or not contain MD files'
+        }
+    return arrayFilesMD
+    };
+//console.log(searchDirectoryWithFilesMD('./index.js'))//devuelve un array vacio si no es md
+
+//Leerlos y validar si dentro de esos archivos MD hay links
+const searchLinksInFilesMD = (route) => {
+    const renderer = new marked.Renderer();
+    let arrayLinks = [];
+     searchDirectoryWithFilesMD(route).forEach((file) => {
+        const readFileMD = readFile(file);
+            renderer.link  = (href, title, text) => {
+            let linksResult = {
+                href: href,
+                text: text,
+                file: file
+            }
+            arrayLinks.push(linksResult)
+        }
+            marked.use({ renderer });
+            marked.parse(readFileMD);
+        });
+        return arrayLinks
+};
+console.log(searchLinksInFilesMD('C:\\Users\\USER\\Desktop\\LIM017-md-links\\prueba\\prueba.md'))
+
+module.exports = {absolutePath, pathExists, isDirectory, isFile, extFile, readDir, readFile, joinTwoPaths,
+                  searchDirectoryWithFilesMD};
