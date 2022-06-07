43472436f12d9011f0521cc750ea6f2eeec0893c
Anggie Bruno Chang <abrunochang@gmail.com>
2022-06-07 17:00:55 -0500

    Implementar funciones para Historia de usuario 1

---
+++ src/index.js
@@ -0,0 +1,18 @@
+import {
+absolutePath,
+yesRoute,
+} from './md-links.js';
+import chalk from 'chalk';
+
+const path = '../pruebas/prueba1.md'
+function mdBig (path){
+  const convertedAbsolutepath = absolutePath(path)
+  if(yesRoute(convertedAbsolutepath)){
+    return'la ruta si existe';
+  }else{
+    return'la ruta no existe';
+  } 
+};
+
+
+console.log(mdBig(path));
